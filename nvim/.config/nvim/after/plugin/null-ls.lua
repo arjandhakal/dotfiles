87f7256d3048e4cf4bd2d1e1a274dffd922943dc
Arjan Dhakal <dhakal.arjan@gmail.com>
2024-02-29 22:14:53 +0545

    enabling eslint only if the root folder has eslint config file

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -4,7 +4,14 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint.with({
+            diagnostics_format = "[eslint] #{m}\n(#{c})",
+            -- enabling only if the root has .eslintrc fule
+            condition = function(utils)
+                return utils.root_has_file(".eslintrc.js") or utils.root_has_file(".eslintrc.cjs") or
+                    utils.root_has_file(".eslintrc.json")
+            end,
+        }),
         null_ls.builtins.completion.spell,
     },
 })
