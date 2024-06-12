d44dd2326775e8e2fff0bb983e630973e1dece00
Arjan Dhakal <dhakal.arjan@gmail.com>
2024-06-12 21:48:35 +0545

    mapping localleader

--- nvim/.config/nvim/lua/arjandhakal/remap.lua
+++ nvim/.config/nvim/lua/arjandhakal/remap.lua
@@ -1,9 +1,7 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- allows you to select lines and move them up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-
-
