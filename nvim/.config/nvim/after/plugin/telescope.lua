e42105ca87194f7674f8630390ed9cf0ee15175c
Arjan Dhakal <dhakal.arjan@gmail.com>
2024-11-22 23:16:20 +0545

    changing remaps

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>.', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>/', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>pd', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>cD', builtin.lsp_references, {})
